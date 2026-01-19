161b0b478bf869153f3945e7c27b8a1a60039c8f
andalagy <157892330+andalagy@users.noreply.github.com>
2026-01-19 00:12:02 -0800

    Move teleop opmodes into manual folder

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/manual/MyTeleOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/manual/MyTeleOp.java
@@ -1,4 +1,4 @@
-package org.firstinspires.ftc.teamcode.opmodes;
+package org.firstinspires.ftc.teamcode.opmodes.manual;
 
 import com.qualcomm.hardware.rev.RevHubOrientationOnRobot;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
