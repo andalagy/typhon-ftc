52b36e0023c6b4e0dc7efffa341e9e93ea07eeea
andalagy <157892330+andalagy@users.noreply.github.com>
2026-02-14 08:27:40 -0800

    Move opmodes to teamcode package

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MyTeleOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MyTeleOp.java
@@ -1,4 +1,4 @@
-package org.firstinspires.ftc.teamcode.opmodes.manual;
+package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.hardware.rev.RevHubOrientationOnRobot;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
